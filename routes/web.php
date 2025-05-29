72f1edbdc40310a1cfbfec4b8434855ce523fe0f
ayeshamargalla <ayeshamp03@gmail.com>
2025-05-30 01:16:20 +0700

    PPL2025-41 UI for Test Assessments Submission from Applicant pov

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,19 @@ use App\Http\Controllers\JobApplicationController;
 use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\RecruiterDashboardController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\UserProfileEditController;
+use App\Http\Controllers\JobPostAssessmentController;
 use App\Models\Notification;
+use App\Models\UserProfile;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/laravel', function () {
     return view('welcome');
 });
+Route::get('/breezedashboard', function () {
+    return view('dashboard');
+})->name('breezedashboard');
 
 
 Route::get('/', [LandingController::class, 'index'])->name('home');
@@ -23,6 +30,9 @@ Route::get('/ajax/jobs', [LandingController::class, 'jobListPartial'])->name('aj
 Route::get('/jobs/{id}/related', [JobPostController::class, 'relatedJobs'])->name('jobs.related');
 
 Route::post('/jobs/{id}/apply', [JobPostController::class, 'apply'])->middleware('auth')->name('jobs.apply');
+Route::get('/assessments/{job}/{application}', [JobPostAssessmentController::class, 'show'])->name('assessments.take');
+Route::post('/jobs/{job}/assessment/submit', [JobPostController::class, 'submitAssessment'])
+        ->name('assessment.submit');
 
 
 Route::middleware(['auth', 'admin'])->group(function () {
@@ -31,7 +41,42 @@ Route::middleware(['auth', 'admin'])->group(function () {
 });
 
 
+Route::resource('articles', ArticleController::class);
+Route::get('/articles', [ArticleController::class, 'index'])->name('articles.index');
+Route::get('/articles/{article}', [ArticleController::class, 'show'])->name('articles.show');
+Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
 
+Route::post('/articles/{id}/comment', [ArticleController::class, 'storeComment'])->name('article.comment');
+
+Route::get('/admin/articles/verification', [ArticleController::class, 'verifyArticles'])->name('admin.articles.verify');
+Route::get('/admin/articles/articlelist', [ArticleController::class, 'listArticles'])->name('admin.articles.articlelist');
+Route::patch('/admin/articles/{id}/approve', [ArticleController::class, 'approveArticle'])->name('admin.articles.approve');
+Route::get('/admin/articles/admin-published', [ArticleController::class, 'adminPublishedArticles'])->name('admin.articles.adminPublished');
+
+Route::middleware(['auth'])->prefix('profile/edit')->group(function () {
+    Route::get('/basic-info', [UserProfileEditController::class, 'edit'])->name('userprofile.edit');
+    Route::post('/basic-info', [UserProfileEditController::class, 'update'])->name('userprofile.update');
+    Route::get('/experience', [UserProfileEditController::class, 'editexperience'])->name('experience.edit');
+    Route::post('/experience', [UserProfileEditController::class, 'updateExperience'])->name('experience.update');
+    Route::get('/skills', [UserProfileEditController::class, 'editSkills'])->name('skills.edit');
+    Route::post('/skills', [UserProfileEditController::class, 'updateSkills'])->name('skills.update');
+    Route::get('/projects', [UserProfileEditController::class, 'editProjects'])->name('projects.edit');
+    Route::post('/projects', [UserProfileEditController::class, 'updateProjects'])->name('projects.update');
+    Route::get('/education', [UserProfileEditController::class, 'editEducation'])->name('education.edit');
+    Route::post('/education', [UserProfileEditController::class, 'updateEducation'])->name('education.update');
+    Route::get('/profile/edit/achievements', [UserProfileEditController::class, 'editAchievements'])->name('achievements.edit');
+    Route::post('/profile/edit/achievements', [UserProfileEditController::class, 'updateAchievements'])->name('achievements.update');
+
+
+
+});
+
+Route::view('/assessment-preview', 'assessment-static');
+Route::view('/assessment-file', 'assessment-file');
+
+
+
+Route::get('/profilepage', [App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show');
 
 
 
@@ -62,10 +107,18 @@ Route::middleware(['auth', 'applicant'])->group(function () {
 });
 
 // Recruiter-only access
-Route::middleware(['auth', 'verified.recruiter'])->group(function () {
+Route::middleware(['auth', 'recruiter'])->group(function () {
     Route::get('/recruiter/profile', [RecruiterController::class, 'showProfileForm'])->name('recruiter.profile');
     Route::post('/recruiter/profile/store', [RecruiterController::class, 'storeProfile'])->name('recruiter.profile.store');
 
+    
+
+});
+    
+
+
+Route::middleware(['auth', 'verified.recruiter'])->group(function () {
+    // Route::get('/', [JobPostController::class, 'index'])->name('jobs.index');
     // Main Dashboard Page Route
     Route::get('/dashboard', [RecruiterDashboardController::class, 'recruiterDashboard'])->name('recruiter.dashboard');
     
@@ -74,16 +127,10 @@ Route::middleware(['auth', 'verified.recruiter'])->group(function () {
     
     Route::post('/recruiter/company/check', [RecruiterController::class, 'checkCompany'])->name('recruiter.company.check');
     Route::post('/recruiter/profile/store', [RecruiterController::class, 'storeProfile'])->name('recruiter.profile.store');
-    Route::get('/recruiter/applications', [RecruiterController::class, 'applications'])->name('applications.index');
+    // Route::get('/recruiter/applications', [RecruiterController::class, 'applications'])->name('applications.index');
+    Route::get('/recruiter/jobs/{id}/applications', [RecruiterController::class, 'applicationsByJob'])->name('recruiter.applications.byJob');
     Route::get('/recruiter/applications/{application}', [JobApplicationController::class, 'show'])->name('applications.show');
     Route::patch('/recruiter/applications/{application}/update-status', [JobApplicationController::class, 'updateStatus'])->name('applications.updateStatus');
-
-});
-    
-
-
-Route::middleware(['auth', 'verified.recruiter'])->group(function () {
-    // Route::get('/', [JobPostController::class, 'index'])->name('jobs.index');
     Route::get('/create', [JobPostController::class, 'create'])->name('jobs.create');
     Route::post('/store', [JobPostController::class, 'store'])->name('jobs.store');
     Route::get('/jobs', [JobPostController::class, 'index'])->name('jobs.index');
